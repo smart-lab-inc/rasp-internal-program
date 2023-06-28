eddd044328313d2fb120ee5e54fe945db36e7dc4
Fernando Guerrero <devrrior@gmail.com>
2023-06-27 23:31:38 -0600

    Update main.py in order to send data to rabbit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# import serial
+import serial
 from rabbitmq import RabbitMQ
-import json
 
 ARDUINO_PORT = "/dev/ttyUSB0"
 RABBITMQ_URL = "54.243.203.221"
@@ -21,13 +20,15 @@ def main():
         RABBITMQ_URL, RABBITMQ_USER, RABBITMQ_PASSWORD, schema=RABBIT_SCHEMA
     )
 
-    # port = serial.Serial(ARDUINO_PORT, 9600)
-    # port.flush()
+    port = serial.Serial(ARDUINO_PORT, 9600)
+    port.flush()
 
-    # while True:
-    #     if port.in_waiting > 0:
-    #         line = port.readline().decode("utf-8").rstrip()
-    #         print(line)
+    while True:
+        if port.in_waiting > 0:
+            line = port.readline().decode("utf-8").rstrip()
+
+            # TODO: Improve hardcoded data
+            rabbit.send("babyWatcher", "new.data", line)
 
 
 if __name__ == "__main__":
